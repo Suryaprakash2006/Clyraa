07ddef61b260d90fdbc8c8f954cdd0b6d41f7efc
Suryaprakash2006 <ullamparthisuryaprakash@gmail.com>
2026-03-21 22:27:33 +0530

    Added initial setup for React application with Vite and Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Toaster } from 'react-hot-toast'
+
+function App() {
+
+  return (
+    <>
+    <Toaster position="top-center" reverseOrder={false} />
+    </>
+  )
+}
+
+export default App
