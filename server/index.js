9cb9ece7b7c9bace92f87afd8e5ade71f3c17e3a
Surya Prakash Ullamparthi <153258741+Suryaprakash2006@users.noreply.github.com>
2026-05-09 20:59:32 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://clyraa-5iei.onrender.com"],
   credentials: true,
 };
 
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://clyraa-5iei.onrender.com"],
     credentials: true,
   },
 });
