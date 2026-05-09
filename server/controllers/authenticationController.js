a5b8fdb17251383c35fc4d0a3e9d1971fbfd4d7d
Surya Prakash Ullamparthi <153258741+Suryaprakash2006@users.noreply.github.com>
2026-05-09 21:16:55 +0530

    Change SameSite cookie attribute to 'none'
    Updated SameSite attribute for cookies to 'none' for cross-site usage.

--- server/controllers/authenticationController.js
+++ server/controllers/authenticationController.js
@@ -83,7 +83,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000
       })
       .json({
@@ -104,7 +104,7 @@ export const logout = (req, res) => {
   try {
     return res.status(200).clearCookie("token", {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENV === "production"
     }).json({ message: "Logged out successfully", success: true });
   } catch (error) {
